e0d49b7a209fdcede6d07f44c17c74f00c35cdde
Michal Porteš <michalportes1@gmail.com>
2021-03-24 12:26:11 +0100

    Move inner function to module level

--- posmatch/core.py
+++ posmatch/core.py
@@ -15,14 +15,6 @@ def pos_match(cls=None, /, *, force=False):
     defined on its own) it will not be set, unless `force` is set to
     True.
     """
-    def set_match_args(cls):
-        init_params = inspect.signature(cls.__init__).parameters
-        param_names = tuple(init_params.keys())
-
-        # do not include the first parameter (self)
-        setattr(cls, '__match_args__', param_names[1:])
-        return cls
-
     if cls:
         # decorator used in the form @pos_match
         return cls if hasattr(cls, '__match_args__') else set_match_args(cls)
@@ -33,3 +25,12 @@ def pos_match(cls=None, /, *, force=False):
 
     # @pos_match(), @pos_match(force=False) or equivalent usage
     return pos_match
+
+
+def set_match_args(cls):
+    init_params = inspect.signature(cls.__init__).parameters
+    param_names = tuple(init_params.keys())
+
+    # do not include the first parameter (self)
+    setattr(cls, '__match_args__', param_names[1:])
+    return cls
