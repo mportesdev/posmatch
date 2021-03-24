7be786c5b48b7aa1200db91d2e2719b9c25f9f71
Michal Porteš <michalportes1@gmail.com>
2021-03-24 11:54:46 +0100

    Add pattern matching tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,3 +1,7 @@
+import sys
+
+import pytest
+
 from posmatch.core import auto_match_args
 
 
@@ -120,3 +124,63 @@ def test_force_override_inherited_match_args():
     instance = SubClass(1, 2)
     assert instance.__match_args__ == ('x', 'y')
     assert SubClass.__base__.__match_args__ == ('a', 'b', 'c')
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason='requires Py 3.10+')
+def test_simple_class_pattern_matching_one_attribute():
+
+    @auto_match_args
+    class Class:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+    from matching import match_first
+
+    instance = Class(1, 2)
+    assert match_first(instance) == 1
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason='requires Py 3.10+')
+def test_simple_class_pattern_matching_two_attributes():
+
+    @auto_match_args
+    class Class:
+        def __init__(self, x, y):
+            self.x = x
+            self.y = y
+
+    from matching import match_two
+
+    instance = Class(x='foo', y=42)
+    assert match_two(instance) == ('foo', 42)
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason='requires Py 3.10+')
+def test_pattern_matching_one_attribute():
+
+    @auto_match_args
+    class Class:
+        def __init__(self, a, /, b, *c, d, e=None, **f):
+            self.a = a
+            self.b = b
+
+    from matching import match_first
+
+    instance = Class(1, 2, d=3)
+    assert match_first(instance) == 1
+
+
+@pytest.mark.skipif(sys.version_info < (3, 10), reason='requires Py 3.10+')
+def test_pattern_matching_two_attributes():
+
+    @auto_match_args
+    class Class:
+        def __init__(self, a, /, b, *c, d, e=None, **f):
+            self.a = a
+            self.b = b
+
+    from matching import match_two
+
+    instance = Class('bar', b=[], d=42)
+    assert match_two(instance) == ('bar', [])
