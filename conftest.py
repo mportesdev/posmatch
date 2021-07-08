25acb1e3b7cd41b20fb4d4fdbb4c155a757db81d
Michal Porteš <michalportes1@gmail.com>
2021-07-08 10:43:16 +0200

    Extract decorator, shorten names

--- conftest.py
+++ conftest.py
@@ -7,42 +7,46 @@ from posmatch import pos_match, PosMatchMeta
 if sys.version_info < (3, 10):
     collect_ignore = ['tests/test_matching.py']
 
+BY_DECORATOR_OR_METACLASS = pytest.fixture(
+    params=['decorator', 'decorator call', 'metaclass']
+)
 
-@pytest.fixture(params=['decorator', 'decorator call', 'metaclass'])
+
+@BY_DECORATOR_OR_METACLASS
 def simple_class(request):
 
     @pos_match
-    class ClassWithDecorator:
+    class WithDecorator:
         def __init__(self, a, b):
             self.a = a
             self.b = b
 
     @pos_match()
-    class ClassWithDecoratorCall:
+    class WithDecoratorCall:
         def __init__(self, a, b):
             self.a = a
             self.b = b
 
-    class ClassWithMetaClass(metaclass=PosMatchMeta):
+    class WithMetaClass(metaclass=PosMatchMeta):
         def __init__(self, a, b):
             self.a = a
             self.b = b
 
     return {
-        'decorator': ClassWithDecorator,
-        'decorator call': ClassWithDecoratorCall,
-        'metaclass': ClassWithMetaClass,
+        'decorator': WithDecorator,
+        'decorator call': WithDecoratorCall,
+        'metaclass': WithMetaClass,
     }[request.param]
 
 
-@pytest.fixture(params=['decorator', 'decorator call', 'metaclass'])
+@BY_DECORATOR_OR_METACLASS
 def six_pack_class(request):
     """Return a class with six kinds of args in signature.
     (positional-only, positional, packed positional, keyword-only,
     keyword, packed keyword)
     """
     @pos_match
-    class ClassWithDecorator:
+    class WithDecorator:
         def __init__(self, a, /, b, *c, d, e=None, **f):
             self.a = a
             self.b = b
@@ -52,7 +56,7 @@ def six_pack_class(request):
             self.f = f
 
     @pos_match()
-    class ClassWithDecoratorCall:
+    class WithDecoratorCall:
         def __init__(self, a, /, b, *c, d, e=None, **f):
             self.a = a
             self.b = b
@@ -61,7 +65,7 @@ def six_pack_class(request):
             self.e = e
             self.f = f
 
-    class ClassWithMetaClass(metaclass=PosMatchMeta):
+    class WithMetaClass(metaclass=PosMatchMeta):
         def __init__(self, a, /, b, *c, d, e=None, **f):
             self.a = a
             self.b = b
@@ -71,18 +75,18 @@ def six_pack_class(request):
             self.f = f
 
     return {
-        'decorator': ClassWithDecorator,
-        'decorator call': ClassWithDecoratorCall,
-        'metaclass': ClassWithMetaClass,
+        'decorator': WithDecorator,
+        'decorator call': WithDecoratorCall,
+        'metaclass': WithMetaClass,
     }[request.param]
 
 
-@pytest.fixture(params=['decorator', 'decorator call', 'metaclass'])
+@BY_DECORATOR_OR_METACLASS
 def class_with_attr(request):
     """Return a class with the `__match_args__` attribute set."""
 
     @pos_match
-    class ClassWithDecorator:
+    class WithDecorator:
         def __init__(self, a, b):
             self.a = a
             self.b = b
@@ -92,7 +96,7 @@ def class_with_attr(request):
         __match_args__ = ('x', 'y')
 
     @pos_match()
-    class ClassWithDecoratorCall:
+    class WithDecoratorCall:
         def __init__(self, a, b):
             self.a = a
             self.b = b
@@ -101,7 +105,7 @@ def class_with_attr(request):
 
         __match_args__ = ('x', 'y')
 
-    class ClassWithMetaClass(metaclass=PosMatchMeta):
+    class WithMetaClass(metaclass=PosMatchMeta):
         def __init__(self, a, b):
             self.a = a
             self.b = b
@@ -111,13 +115,13 @@ def class_with_attr(request):
         __match_args__ = ('x', 'y')
 
     return {
-        'decorator': ClassWithDecorator,
-        'decorator call': ClassWithDecoratorCall,
-        'metaclass': ClassWithMetaClass,
+        'decorator': WithDecorator,
+        'decorator call': WithDecoratorCall,
+        'metaclass': WithMetaClass,
     }[request.param]
 
 
-@pytest.fixture(params=['decorator', 'decorator call', 'metaclass'])
+@BY_DECORATOR_OR_METACLASS
 def class_with_inherited(request):
     """Return a class with the `__match_args__` attribute inherited."""
 
@@ -129,29 +133,29 @@ def class_with_inherited(request):
         __match_args__ = ('a', 'b')
 
     @pos_match
-    class ClassWithDecorator(BaseClass):
+    class WithDecorator(BaseClass):
         def __init__(self, x, y):
             super().__init__(x, y)
             self.x = self.a + 42
             self.y = self.b + 42
 
     @pos_match()
-    class ClassWithDecoratorCall(BaseClass):
+    class WithDecoratorCall(BaseClass):
         def __init__(self, x, y):
             super().__init__(x, y)
             self.x = self.a + 42
             self.y = self.b + 42
 
-    class ClassWithMetaClass(BaseClass, metaclass=PosMatchMeta):
+    class WithMetaClass(BaseClass, metaclass=PosMatchMeta):
         def __init__(self, x, y):
             super().__init__(x, y)
             self.x = self.a + 42
             self.y = self.b + 42
 
     return {
-        'decorator': ClassWithDecorator,
-        'decorator call': ClassWithDecoratorCall,
-        'metaclass': ClassWithMetaClass,
+        'decorator': WithDecorator,
+        'decorator call': WithDecoratorCall,
+        'metaclass': WithMetaClass,
     }[request.param]
 
 
@@ -160,7 +164,7 @@ def forced_class(request):
     """Return a class decorated with `@pos_match(force=True)`."""
 
     @pos_match(force=True)
-    class ClassWithOwnMatchArgs:
+    class WithOwnMatchArgs:
         def __init__(self, a, b):
             self.a = a
             self.b = b
@@ -177,13 +181,13 @@ def forced_class(request):
         __match_args__ = ('x', 'y')
 
     @pos_match(force=True)
-    class ClassWithInheritedMatchArgs(BaseClass):
+    class WithInheritedMatchArgs(BaseClass):
         def __init__(self, a, b):
             super().__init__(a, b)
             self.a = self.x + 42
             self.b = self.y + 42
 
     return {
-        'own': ClassWithOwnMatchArgs,
-        'inherited': ClassWithInheritedMatchArgs,
+        'own': WithOwnMatchArgs,
+        'inherited': WithInheritedMatchArgs,
     }[request.param]
