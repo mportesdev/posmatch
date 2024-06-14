1cc6415f3fda2ced45400e852e0f8276a02d7b42
Michal Porteš <michalportes1@gmail.com>
2024-06-14 08:55:07 +0200

    Bump version to 0.8.1

--- posmatch/__init__.py
+++ posmatch/__init__.py
@@ -1,5 +1,5 @@
 from posmatch.core import PosMatchMeta, PosMatchMixin, pos_match
 
-__version__ = "0.8.0"
+__version__ = "0.8.1"
 
 __all__ = ["__version__", "pos_match", "PosMatchMeta", "PosMatchMixin"]
